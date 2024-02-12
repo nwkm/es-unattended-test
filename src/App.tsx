732b39b3e6dc394d17f6ab54114e11ba04787895
nawin <kim.nawin@gmail.com>
2024-02-13 00:44:04 +0100

    fix ghpage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import PlayerList from './pages/PlayerList';
 import WinDetails from './pages/WinDetailList';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter basename="/">
             <Routes>
                 <Route path="/" element={<PlayerList />} />
                 <Route path="/players/:id" element={<WinDetails />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
